3e35fcb616ab3b294bfc1493240a7f9ad64fa619
Weranry <sliawry@emails.imau.edu.cn>
2025-04-23 23:38:28 +0800

    shit

--- lib/Weather_landscape/weather_landscape_fetcher.py
+++ lib/Weather_landscape/weather_landscape_fetcher.py
@@ -2,6 +2,8 @@ import json
 import datetime
 import requests
 import pytz
+from math import cos, sin, acos, asin, tan
+from math import degrees as deg, radians as rad
 
 class WeatherData:
     """整合获取天气数据的功能"""
@@ -35,9 +37,7 @@ class WeatherData:
         self.pressure_min = pressure_min
         self.pressure_max = pressure_max
         self.weather_data = []
-        # 使用pytz处理时区
-        self.local_tz = pytz.timezone('Asia/Shanghai')  # 默认使用中国时区，可以根据需要修改
-        self.tzoffset = datetime.datetime.now(self.local_tz).utcoffset().total_seconds()/(60*60)
+        self.tzoffset = (datetime.datetime.now() - datetime.datetime.utcnow()).total_seconds()/(60*60)
         
         # 构建API请求URL
         self.reqstr = f"lat={self.lat}&lon={self.lon}&mode=json&APPID={self.api_key}"
@@ -71,12 +71,8 @@ class WeatherData:
         """处理从API获取的天气数据"""
         is_celsius = self.units_mode != self.TEMP_UNITS_FAHRENHEIT
         
-        # 使用pytz将UTC时间转换为本地时间
-        utc_time = datetime.datetime.utcfromtimestamp(int(data['dt']))
-        local_time = utc_time.replace(tzinfo=pytz.utc).astimezone(self.local_tz)
-        
         weather_info = {
-            'time': local_time,
+            'time': datetime.datetime.fromtimestamp(int(data['dt'])),
             'id': int(data['weather'][0]['id']),
             'clouds': int(data['clouds'].get('all', 0)) if 'clouds' in data else 0,
             'rain': 0.0,
@@ -131,33 +127,30 @@ class WeatherData:
             return False
         return True
     
+    def ensure_localized(self, dt):
+        if dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None:
+            return pytz.timezone('Asia/Shanghai').localize(dt)
+        return dt
+    
     def get_temp_range(self, max_time):
-        """获取指定时间内的温度范围"""
+        """获取指定时间内的温度范围（时区安全）"""
         if len(self.weather_data) == 0:
             return None
-
-        # Ensure max_time is offset-aware
-        if max_time.tzinfo is None or max_time.tzinfo.utcoffset(max_time) is None:
-            max_time = self.local_tz.localize(max_time)
-
+        max_time = self.ensure_localized(max_time)
         tmax = -999
         tmin = 999
         is_first = True
-
         for weather in self.weather_data:
             if is_first:
                 is_first = False
                 continue
-
-            if weather['time'] > max_time:
+            weather_time = self.ensure_localized(weather['time'])
+            if weather_time > max_time:
                 break
-
             if weather['temp'] > tmax:
                 tmax = weather['temp']
-
             if weather['temp'] < tmin:
                 tmin = weather['temp']
-
         return (tmin, tmax)
     
     def get_current(self):
@@ -168,11 +161,86 @@ class WeatherData:
     
     def get_forecast_at_time(self, time):
         """获取指定时间的天气预报"""
-        # Ensure time is offset-aware
-        if time.tzinfo is None or time.tzinfo.utcoffset(time) is None:
-            time = self.local_tz.localize(time)
-
         for weather in self.weather_data:
             if weather['time'] > time:
                 return weather
         return None
+
+
+class SunCalculator:
+    """计算日出日落时间"""
+    
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+        self.tzoffset = (datetime.datetime.now() - datetime.datetime.utcnow()).total_seconds()/(60*60)
+    
+    def sunrise(self, when=None):
+        """计算日出时间"""
+        if when is None:
+            when = datetime.datetime.now()
+        self.__preptime(when)
+        self.__calc()
+        return self.__timefromdecimalday(self.sunrise_t, when)
+    
+    def sunset(self, when=None):
+        """计算日落时间"""
+        if when is None:
+            when = datetime.datetime.now()
+        self.__preptime(when)
+        self.__calc()
+        return self.__timefromdecimalday(self.sunset_t, when)
+    
+    def solarnoon(self, when=None):
+        """计算正午时间"""
+        if when is None:
+            when = datetime.datetime.now()
+        self.__preptime(when)
+        self.__calc()
+        return self.__timefromdecimalday(self.solarnoon_t, when)
+    
+    @staticmethod
+    def __timefromdecimalday(day, when):
+        hours = 24.0 * day
+        h = int(hours)
+        minutes = (hours - h) * 60
+        m = int(minutes)
+        seconds = (minutes - m) * 60
+        s = int(seconds)
+        return datetime.datetime(when.year, when.month, when.day, h, m, s)
+    
+    def __preptime(self, when):
+        self.day = when.toordinal() - (734124 - 40529)
+        t = when.time()
+        self.time = (t.hour + t.minute/60.0 + t.second/3600.0) / 24.0
+        self.timezone = self.tzoffset
+    
+    def __calc(self):
+        timezone = self.timezone  # in hours, east is positive
+        longitude = self.lon      # in decimal degrees, east is positive
+        latitude = self.lat       # in decimal degrees, north is positive
+        
+        time = self.time  # percentage past midnight, i.e. noon is 0.5
+        day = self.day    # daynumber 1=1/1/1900
+        
+        Jday = day + 2415018.5 + time - timezone / 24  # Julian day
+        Jcent = (Jday - 2451545) / 36525    # Julian century
+        
+        Manom = 357.52911 + Jcent * (35999.05029 - 0.0001537 * Jcent)
+        Mlong = 280.46646 + Jcent * (36000.76983 + Jcent * 0.0003032) % 360
+        Eccent = 0.016708634 - Jcent * (0.000042037 + 0.0001537 * Jcent)
+        Mobliq = 23 + (26 + ((21.448 - Jcent * (46.815 + Jcent * (0.00059 - Jcent * 0.001813)))) / 60) / 60
+        obliq = Mobliq + 0.00256 * cos(rad(125.04 - 1934.136 * Jcent))
+        vary = tan(rad(obliq / 2)) * tan(rad(obliq / 2))
+        Seqcent = sin(rad(Manom)) * (1.914602 - Jcent * (0.004817 + 0.000014 * Jcent)) + sin(rad(2 * Manom)) * (0.019993 - 0.000101 * Jcent) + sin(rad(3 * Manom)) * 0.000289
+        Struelong = Mlong + Seqcent
+        Sapplong = Struelong - 0.00569 - 0.00478 * sin(rad(125.04 - 1934.136 * Jcent))
+        declination = deg(asin(sin(rad(obliq)) * sin(rad(Sapplong))))
+        
+        eqtime = 4 * deg(vary * sin(2 * rad(Mlong)) - 2 * Eccent * sin(rad(Manom)) + 4 * Eccent * vary * sin(rad(Manom)) * cos(2 * rad(Mlong)) - 0.5 * vary * vary * sin(4 * rad(Mlong)) - 1.25 * Eccent * Eccent * sin(2 * rad(Manom)))
+        
+        hourangle = deg(acos(cos(rad(90.833)) / (cos(rad(latitude)) * cos(rad(declination))) - tan(rad(latitude)) * tan(rad(declination))))
+        
+        self.solarnoon_t = (720 - 4 * longitude - eqtime + timezone * 60) / 1440
+        self.sunrise_t = self.solarnoon_t - hourangle * 4 / 1440
+        self.sunset_t = self.solarnoon_t + hourangle * 4 / 1440
